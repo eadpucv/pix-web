787c95478ef925dcb43cf4c52d16fb971cfa8fb4
Hugo Solar <hugo.solar@gmail.com>
2020-12-23 10:11:50 -0300

    style fix

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,4 +1,5 @@
 import React from "react"
+import Helmet from 'react-helmet'
 import { MDXProvider } from "@mdx-js/react"
 import Header from "../components/header/header"
 import Footer from "../components/footer/footer"
@@ -6,12 +7,18 @@ import Hero from "../components/content/hero"
 import Card from "../components/content/card"
 import Container from "../components/content/container"
 import Button from "../components/elements/button"
+import Image from "../components/elements/image"
 import Contributors from "../components/content/contributors"
 
-const shortcodes = { Header, Footer, Hero, Card, Button, Container, Contributors }
-export default function Layout({ children }) {
+const shortcodes = { Header, Footer, Hero, Card, Button, Container, Contributors, Image }
+export default function Layout({ location, children }) {
   return (
     <MDXProvider components={shortcodes}>
+      <Helmet
+        bodyAttributes={{
+          class: location.pathname.replace(/\//g,'')
+        }}
+      />
       <Header />
       <section className="main-content">
         {children}
