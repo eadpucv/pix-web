d12e258f7ab0256a6f9c96429b3ded51857722b9
Hugo Solar <hugo.solar@gmail.com>
2021-05-31 14:14:13 -0400

    remove debug message

--- src/components/elements/profile.js
+++ src/components/elements/profile.js
@@ -1,7 +1,6 @@
 import React from "react"
 
 export function Profile({src, alt, to, ...imageProps}) {
-  console.log('lalalal')
   return(
     <a href={to} target="_blank" rel="noreferrer" className={`image profile`}>
       <img src={src} alt={alt} {...imageProps} />
